eac66f0c55e69dff911c28321415eb944db62056
JMJ Riyana <jeemariyana@gmail.com>
2020-02-16 12:23:15 +0530

    end

--- bigsort.c
+++ bigsort.c
@@ -11,6 +11,35 @@ int main(){
 
     unsigned long int i,j,a,n,number[100000000000000000];
     scanf("%lu",&n);
+
+    
+        for (i = 0; i < n; ++i)
+            scanf("%lu",&number[i]);
+ 
+        for (i = 0; i < n; ++i) 
+        {
+ 
+            for (j = i + 1; j < n; ++j)
+            {
+ 
+                if (number[i] > number[j]) 
+                {
+ 
+                    a =  number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+        for (i = 0; i < n; ++i){
+            printf("%lu\n", number[i]);
+        }
+
  
 
     return 0;
+}
