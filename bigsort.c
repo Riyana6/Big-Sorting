0f4af66190fd9022c4cd15f0eb8e4e1ad0b3b9eb
JMJ Riyana <jeemariyana@gmail.com>
2020-02-16 12:20:24 +0530

    initializing

---
+++ bigsort.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+
+    unsigned long int i,j,a,n,number[100000000000000000];
+    scanf("%lu",&n);
+ 
+
+    return 0;
